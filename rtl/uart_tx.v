ffe70a1844a92e93e8aba01c7128e1c639936350
Viraj Kumar <kumar.viraj2019@gmail.com>
2026-06-30 18:56:53 +0530

    Update and rename uart_tx.v to rtl/uart_tx.v

--- rtl/uart_tx.v
+++ rtl/uart_tx.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 19.06.2026 03:21:44
-// Design Name: 
-// Module Name: uart_tx
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module uart_tx(input clk,
                input rst,
