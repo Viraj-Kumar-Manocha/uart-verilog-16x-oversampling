616373105504125b6685b32ab4a8caab2b39ab6b
Viraj Kumar <kumar.viraj2019@gmail.com>
2026-06-30 18:55:16 +0530

    Update and rename baud_rate_gen_16x.v to rtl/baud_rate_gen_16x.v

--- rtl/baud_rate_gen_16x.v
+++ rtl/baud_rate_gen_16x.v
@@ -1,30 +1,10 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 20.06.2026 03:41:18
-// Design Name: 
-// Module Name: baud_rate_gen_16x
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module baud_rate_gen_16x #(parameter CLKS_PER_BIT = 5208)
                         (input clk,
                          input rst,
                          output reg tick);
-localparam CLKS_PER_TICK = (CLKS_PER_BIT+8) /16; //if directly /16 it truncates,but now it rounds off                        
+  localparam CLKS_PER_TICK = (CLKS_PER_BIT+8) /16; //if directly divide by 16 it truncates,but now it rounds off                        
 localparam WIDTH = $clog2(CLKS_PER_TICK);
 reg [WIDTH-1:0]count;
 
