28d00ec0763fe21b612ae1df27fdd0d1d4404efb
Viraj Kumar <kumar.viraj2019@gmail.com>
2026-07-01 19:37:49 +0530

    Update and rename testbench_uart_transmitter.v to tb/testbench_uart_transmitter.v

--- tb/testbench_uart_transmitter.v
+++ tb/testbench_uart_transmitter.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 20.06.2026 02:09:00
-// Design Name: 
-// Module Name: testbench_uart_transmitter
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module testbench_uart_transmitter;
 reg clk = 1'b0;
