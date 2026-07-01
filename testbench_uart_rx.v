cd1b621c3b0d4b2d440c9681434a2ec16fb241a0
Viraj Kumar <kumar.viraj2019@gmail.com>
2026-07-01 19:36:54 +0530

    .

---
+++ testbench_uart_rx.v
@@ -0,0 +1,76 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 20.06.2026 21:27:45
+// Design Name: 
+// Module Name: testbench_uart_rx
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module testbench_uart_rx;
+reg clk = 1'b0;
+reg rst = 1'b1;
+reg rx = 1'b1;
+wire tick;
+integer i = 0;
+reg [7:0] data_in=8'hA5;
+
+wire [7:0] data_out;
+wire data_valid;
+
+baud_rate_gen_16x #(.CLKS_PER_BIT(160)) test(.clk(clk),.rst(rst),.tick(tick));
+
+uart_rx uut(.clk(clk),.rst(rst),.tick(tick),.rx(rx),.data_out(data_out),.data_valid(data_valid));
+
+parameter BIT_TIME = 1600;
+always #5 clk = ~ clk;
+
+initial begin
+    #50;
+    rst = 1'b0;
+    #1000;
+    rx = 1'b0;
+    #(BIT_TIME);
+    for (i=0;i<8;i=i+1) begin
+            rx = data_in[i];
+            #(BIT_TIME);
+    end
+    rx = 1'b1;
+    #5000;
+    
+    rx = 1'b0;
+    #(BIT_TIME);
+    data_in = 8'hC3;
+    for (i=0;i<8;i=i+1) begin
+        rx = data_in[i];
+        #(BIT_TIME);
+    end
+    rx = 1'b1;
+    #5000;
+    
+    rx=1'b0;
+    #(BIT_TIME);
+    data_in = 8'hf1;
+    for (i=0;i<8;i=i+1) begin
+        rx = data_in[i];
+        #(BIT_TIME);
+    end  
+    rx=1'b0;
+    #20000; 
+      //not making rx = 1'b1 for checking error correctly shows
+    $finish;
+    end
+endmodule
---
+++ testbench_uart_transmitter.v
@@ -0,0 +1,78 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 20.06.2026 02:09:00
+// Design Name: 
+// Module Name: testbench_uart_transmitter
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module testbench_uart_transmitter;
+reg clk = 1'b0;
+reg tx_start = 1'b0;
+reg rst=1'b0;
+reg tick = 1'b0;
+reg [7:0] data_in;
+wire out,busy;
+
+uart_tx tb(.clk(clk),.rst(rst),.tick(tick),.tx_start(tx_start),.data_in(data_in),.tx(out),.busy(busy));
+
+always #5 clk = ~clk;
+
+always begin 
+    tick = 1'b1;
+    #10;
+    tick = 1'b0;
+    #90;
+end
+    
+initial begin
+    rst = 1'b1;
+    #50;
+    rst = 1'b0;
+    
+    //first transmission
+    tx_start = 1'b1;
+    data_in = 8'hA5;
+    #10;
+    tx_start = 1'b0;
+    
+    data_in = 8'hF1;  //changing data mid tranmission to check busy and data_reg
+    
+    #2000;  //waiting
+    
+    //second transmission 
+    tx_start = 1'b1;
+    #10;
+    tx_start = 1'b0;
+    
+    #500; //wait, less because checking whether transmission starts when busy
+    
+    //third transmission
+    tx_start = 1'b1;
+    data_in = 8'h3C;
+    #10;
+    tx_start = 1'b0;
+    
+    #800;
+    tx_start = 1'b1;
+    #10;
+    tx_start = 1'b0;
+    
+    #1000; //wait
+    $finish;
+end     
+endmodule
