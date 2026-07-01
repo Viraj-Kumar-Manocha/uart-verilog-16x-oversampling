8671de985b23073254ccefaf7915350fd20a6bcd
Viraj Kumar <kumar.viraj2019@gmail.com>
2026-07-01 19:37:24 +0530

    Update and rename testbench_uart_rx.v to tb/testbench_uart_rx.v

--- tb/testbench_uart_rx.v
+++ tb/testbench_uart_rx.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 20.06.2026 21:27:45
-// Design Name: 
-// Module Name: testbench_uart_rx
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module testbench_uart_rx;
 reg clk = 1'b0;
