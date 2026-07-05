bb0293738bd24368be2c5feb4919f9dda4edf63f
Viraj Kumar <kumar.viraj2019@gmail.com>
2026-07-05 16:43:35 +0530

    Update testbench_uart_2.v

--- tb/testbench_uart_2.v
+++ tb/testbench_uart_2.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 01.07.2026 02:05:15
-// Design Name: 
-// Module Name: testbench_uart_2
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module testbench_uart_2;
 reg clk = 1'b0;
